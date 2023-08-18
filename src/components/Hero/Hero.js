57340bf6fd8858b17405a23353ba1efa68421dc3
OmeshSachethana <sachethana12@gmail.com>
2023-08-18 13:51:19 +0530

    changed photo

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,12 @@ const Hero = (props) => (
           Welcome To <br />
           My Personal Portfolio
       </SectionTitle>
-      <img src="../images/profile.png" alt="Profile" style={{ borderRadius: '12px',}}/> <br /><br />
+      <img
+        src="../images/profile_photo.jpg"
+        alt="Profile"
+        style={{ maxWidth: '40%', borderRadius: '12px' }}
+      />
+      <br /><br />
       <SectionText>My name is Omesh Sachethana. I am currently a 3rd-year 2nd-semester student pursuing a degree in Information Technology specialization in Software Engineering at Sri Lanka Institute of Information Technology (SLIIT). As a student in this field, I am likely to be skilled in programming languages, software development methodologies, and other technical skills necessary for designing, developing, and testing software applications. 
         I also possess critical thinking skills, problem-solving abilities, and creativity required to excel in my field.</SectionText>
       <Button onClick={() => window.location = 'https://drive.google.com/file/d/1RyK1RbqTKXLZ1A7Yqk2vvnFyN3n4enij/view?usp=sharing'} >Learn More</Button>
