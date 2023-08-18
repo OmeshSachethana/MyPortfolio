b6e8d5dfd329519edeab40bfa15736c898c032c8
OmeshSachethana <sachethana12@gmail.com>
2023-08-18 12:55:32 +0530

    updated link cv

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
       <img src="../images/profile.png" alt="Profile" style={{ borderRadius: '12px',}}/> <br /><br />
       <SectionText>My name is Omesh Sachethana. I am currently a 3rd-year 2nd-semester student pursuing a degree in Information Technology specialization in Software Engineering at Sri Lanka Institute of Information Technology (SLIIT). As a student in this field, I am likely to be skilled in programming languages, software development methodologies, and other technical skills necessary for designing, developing, and testing software applications. 
         I also possess critical thinking skills, problem-solving abilities, and creativity required to excel in my field.</SectionText>
-      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1qVfbZd19g1N1j-2VCOlgqDBsoSibC2kb/view?usp=sharing'} >Learn More</Button>
+      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1RyK1RbqTKXLZ1A7Yqk2vvnFyN3n4enij/view?usp=sharing'} >Learn More</Button>
     </LeftSection>
   </Section>
 );
