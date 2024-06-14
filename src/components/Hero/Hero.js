dfdff16b46749c4e27ecfea69402ed44ceb01bb3
Omesh Sachethana <126751265+OmeshSachethana@users.noreply.github.com>
2024-06-14 22:01:32 +0530

    Changed academic year

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = (props) => (
         style={{ maxWidth: '40%', borderRadius: '12px' }}
       />
       <br /><br />
-      <SectionText>My name is Omesh Sachethana. I am currently a 3rd-year 2nd-semester student pursuing a degree in Information Technology specialization in Software Engineering at Sri Lanka Institute of Information Technology (SLIIT). As a student in this field, I am likely to be skilled in programming languages, software development methodologies, and other technical skills necessary for designing, developing, and testing software applications. 
+      <SectionText>My name is Omesh Sachethana. I am currently a 4th-year 1st-semester student pursuing a degree in Information Technology specialization in Software Engineering at Sri Lanka Institute of Information Technology (SLIIT). As a student in this field, I am likely to be skilled in programming languages, software development methodologies, and other technical skills necessary for designing, developing, and testing software applications. 
         I also possess critical thinking skills, problem-solving abilities, and creativity required to excel in my field.</SectionText>
       <Button onClick={() => window.location = 'https://drive.google.com/file/d/1jWavKV7GnENdiHnLdCttT7OVDU0q4z4u/view?usp=sharing'} >Learn More</Button>
     </LeftSection>
